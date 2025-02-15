0a140012aedb9397fdec0f51993c9e1d9d591df4
ab07a <bishtavneet1234@gmail.com>
2025-02-15 20:51:14 +0530

    15/02/2025

---
+++ 2698. Find the Punishment Number of an Integer/FindthePunishmentNumberofanInteger.java
@@ -0,0 +1,69 @@
+public class FindthePunishmentNumberofanInteger {
+    public boolean findPartitions(
+        int startIndex,
+        int sum,
+        String stringNum,
+        int target,
+        int[][] memo
+    ) {
+        if (startIndex == stringNum.length()) {
+            return sum == target;
+        }
+        if (sum > target) return false;
+
+        if (memo[startIndex][sum] != -1) return memo[startIndex][sum] == 1;
+
+        boolean partitionFound = false;
+
+        for (
+            int currentIndex = startIndex;
+            currentIndex < stringNum.length();
+            currentIndex++
+        ) {
+            String currentString = stringNum.substring(
+                startIndex,
+                currentIndex + 1
+            );
+            int addend = Integer.parseInt(currentString);
+
+            partitionFound =
+                partitionFound ||
+                findPartitions(
+                    currentIndex + 1,
+                    sum + addend,
+                    stringNum,
+                    target,
+                    memo
+                );
+            if (partitionFound) {
+                memo[startIndex][sum] = 1;
+                return true;
+            }
+        }
+
+        memo[startIndex][sum] = 0;
+        return false;
+    }
+
+    public int punishmentNumber(int n) {
+        int punishmentNum = 0;
+        for (int currentNum = 1; currentNum <= n; currentNum++) {
+            int squareNum = currentNum * currentNum;
+            String stringNum = Integer.toString(squareNum);
+
+            int[][] memoArray = new int[stringNum.length()][currentNum + 1];
+            for (int[] row : memoArray) {
+                java.util.Arrays.fill(row, -1);
+            }
+            if (findPartitions(0, 0, stringNum, currentNum, memoArray)) {
+                punishmentNum += squareNum;
+            }
+        }
+        return punishmentNum;
+    }
+
+    public static void main(String[] args) {
+        FindthePunishmentNumberofanInteger fInteger = new FindthePunishmentNumberofanInteger();
+        System.out.println(fInteger.punishmentNumber(37));
+    }
+}
